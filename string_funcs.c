c2ddea29648ebc903b5c14b33d2d03361028f953
Marina1997Melad <meladmarina8@gmail.com>
2023-09-18 12:05:52 -0700

    shell

--- string_funcs.c
+++ string_funcs.c
@@ -1,5 +1,9 @@
 #include "shell.h"
-
+/**
+ * _strlen - returns the length of string
+ * @s:pointer to string
+ * Return:number of characters in the string 
+ */
 
 int _strlen(char *s)
 {
@@ -12,6 +16,15 @@ int _strlen(char *s)
 
 	return (i);
 }
+/**
+ * _strcmp - compare two strings
+ * @s1:pointer to first string
+ * @s2:pointer to second string
+ * Description: this function compares characters 
+ * of two strings
+ * Return: the difference between the first mismatching
+ * pair of characters or 0 if equal
+ */
 
 int _strcmp(char *s1, char *s2)
 {
@@ -28,7 +41,13 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (*s1 - *s2);
 }
-
+/**
+ * _strcpy_c - copies the string pointed by src to the 
+ * buffer pointed by input
+ * @input: pointer to the destination string
+ * @src:pointer to the source string
+ * Return: pointer to the destination string input
+ */
 char *_strcpy_c(char *input, char *src)
 {
 	int i = 0;
@@ -44,12 +63,20 @@ char *_strcpy_c(char *input, char *src)
 
 	return (s);
 }
-
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: 1 on success and -1 on error
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
+/**
+ * _puts - writes a string to stdout
+ * @s: the string to print
+ * Return: 0 on success
+ */
 int _puts(char *s)
 {
 	int i = 0;
